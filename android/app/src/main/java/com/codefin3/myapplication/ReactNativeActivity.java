4bd4a65c2baa6ad864a435eb1053110af0b206d4
Sittisuk <sittisuk@codefin.io>
2019-03-27 10:41:12 +0700

    add class

---
+++ android/app/src/main/java/com/codefin3/myapplication/ReactNativeActivity.java
@@ -0,0 +1,6 @@
+package com.codefin3.myapplication;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactNativeActivity extends ReactActivity {
+}
---
+++ android/app/src/main/java/com/codefin3/myapplication/ReactNativeApplication.java
@@ -0,0 +1,7 @@
+package com.codefin3.myapplication;
+
+import android.app.Application;
+import com.facebook.react.ReactApplication;
+
+public class ReactNativeApplication extends Application implements ReactApplication {
+}
