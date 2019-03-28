f038d412da031286d8bd6949771e01d4596d64a8
Sittisuk <sittisuk@codefin.io>
2019-03-28 16:14:28 +0700

    can run

---
+++ ios/ios/ios/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by codefin3 on 3/28/19.
+//  Copyright © 2019 codefin3. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
