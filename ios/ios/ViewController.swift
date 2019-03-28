951aca4f47832557c986e3351d39d54d0df210b3
Sittisuk <sittisuk@codefin.io>
2019-03-28 16:38:32 +0700

    connect react native success

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -17,5 +17,28 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func thebutton(_ sender: Any) {
+        NSLog("Hello")
+        let providerSettings = RCTBundleURLProvider.sharedSettings() // release
+        let jsCodeLocation = providerSettings?.jsBundleURL(forBundleRoot: "main.jsbundle", fallbackResource: nil) // release
+        //  let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios") // debug
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+        
+    }
 }
 
